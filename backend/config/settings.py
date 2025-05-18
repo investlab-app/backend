4c37f15aa0480e3f27d0da0c53bb2d39a2f9ea8f
Jakub Stępkowski <stepkos@wp.pl>
2025-05-18 03:15:51 +0200

    feat: add more extra options to unfold admin

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,6 +14,9 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+# from django.templatetags.static import static
+# from django.utils.translation import gettext_lazy as _
+
 from config import str_to_bool, str_to_list
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -200,4 +203,137 @@ AUTH_USER_MODEL = "users.User"
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
+}
+
+UNFOLD = {
+    "SITE_TITLE": "Stocks",
+    "SITE_HEADER": "Stocks Admin Panel",
+    # "SITE_SUBHEADER": "Appears under SITE_HEADER",
+    "SITE_DROPDOWN": [
+        {
+            "icon": "folder",
+            "title": "Github",
+            "link": "https://github.com/wall-street-stocks/stocks-backend",
+        },
+        {
+            "icon": "dashboard",
+            "title": "Frontend",
+            "link": "https://example.com",
+        },
+
+    ],
+    # "SITE_URL": "/",
+    # # "SITE_ICON": lambda request: static("icon.svg"),  # both modes, optimise for 32px height
+    # "SITE_ICON": {
+    #     "light": lambda request: static("icon-light.svg"),  # light mode
+    #     "dark": lambda request: static("icon-dark.svg"),  # dark mode
+    # },
+    # # "SITE_LOGO": lambda request: static("logo.svg"),  # both modes, optimise for 32px height
+    # "SITE_LOGO": {
+    #     "light": lambda request: static("logo-light.svg"),  # light mode
+    #     "dark": lambda request: static("logo-dark.svg"),  # dark mode
+    # },
+    # "SITE_SYMBOL": "speed",  # symbol from icon set
+    # "SITE_FAVICONS": [
+    #     {
+    #         "rel": "icon",
+    #         "sizes": "32x32",
+    #         "type": "image/svg+xml",
+    #         "href": lambda request: static("favicon.svg"),
+    #     },
+    # ],
+    # "SHOW_HISTORY": True,  # show/hide "History" button, default: True
+    # "SHOW_VIEW_ON_SITE": True,  # show/hide "View on site" button, default: True
+    # "SHOW_BACK_BUTTON": False,  # show/hide "Back" button on changeform in header, default: False
+    # "ENVIRONMENT": "sample_app.environment_callback",  # environment name in header
+    # "ENVIRONMENT_TITLE_PREFIX": "sample_app.environment_title_prefix_callback",  # environment name prefix in title tag
+    # "DASHBOARD_CALLBACK": "sample_app.dashboard_callback",
+    # "LOGIN": {
+    #     "image": lambda request: static("sample/login-bg.jpg"),
+    #     "redirect_after": lambda request: reverse_lazy("admin:APP_MODEL_changelist"),
+    # },
+    # "COLORS": {
+    #     "base": {
+    #         "50": "249, 250, 251",
+    #         "100": "243, 244, 246",
+    #         "200": "229, 231, 235",
+    #         "300": "209, 213, 219",
+    #         "400": "156, 163, 175",
+    #         "500": "107, 114, 128",
+    #         "600": "75, 85, 99",
+    #         "700": "55, 65, 81",
+    #         "800": "31, 41, 55",
+    #         "900": "17, 24, 39",
+    #         "950": "3, 7, 18",
+    #     },
+    #     "primary": {
+    #         "50": "250, 245, 255",
+    #         "100": "243, 232, 255",
+    #         "200": "233, 213, 255",
+    #         "300": "216, 180, 254",
+    #         "400": "192, 132, 252",
+    #         "500": "168, 85, 247",
+    #         "600": "147, 51, 234",
+    #         "700": "126, 34, 206",
+    #         "800": "107, 33, 168",
+    #         "900": "88, 28, 135",
+    #         "950": "59, 7, 100",
+    #     },
+    #     "font": {
+    #         "subtle-light": "var(--color-base-500)",  # text-base-500
+    #         "subtle-dark": "var(--color-base-400)",  # text-base-400
+    #         "default-light": "var(--color-base-600)",  # text-base-600
+    #         "default-dark": "var(--color-base-300)",  # text-base-300
+    #         "important-light": "var(--color-base-900)",  # text-base-900
+    #         "important-dark": "var(--color-base-100)",  # text-base-100
+    #     },
+    # },
+    # "EXTENSIONS": {
+    #     "modeltranslation": {
+    #         "flags": {
+    #             "en": "🇬🇧",
+    #             "fr": "🇫🇷",
+    #             "nl": "🇧🇪",
+    #         },
+    #     },
+    # },
+    "SIDEBAR": {
+        "show_search": True,  # Search in applications and models names
+        "show_all_applications": False,  # Dropdown with all applications and models
+        # "navigation": [
+        #     {
+        #         "title": _("Navigation"),
+        #         "separator": True,  # Top border
+        #         "collapsible": True,  # Collapsible group of links
+        #         "items": [
+        #             {
+        #                 "title": _("Dashboard"),
+        #                 "icon": "dashboard",  # Supported icon set: https://fonts.google.com/icons
+        #                 "link": reverse_lazy("admin:index"),
+        #                 "badge": "sample_app.badge_callback",
+        #                 "permission": lambda request: request.user.is_superuser,
+        #             },
+        #             {
+        #                 "title": _("Users"),
+        #                 "icon": "people",
+        #                 "link": reverse_lazy("admin:auth_user_changelist"),
+        #             },
+        #         ],
+        #     },
+        # ],
+    },
+    # "TABS": [
+    #     {
+    #         "models": [
+    #             "app_label.model_name_in_lowercase",
+    #         ],
+    #         "items": [
+    #             {
+    #                 "title": _("Your custom title"),
+    #                 "link": reverse_lazy("admin:app_label_model_name_changelist"),
+    #                 "permission": "sample_app.permission_callback",
+    #             },
+    #         ],
+    #     },
+    # ],
 }
