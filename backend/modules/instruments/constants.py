38e5ec1b06faa12e2130dedded1c5371a854232a
Jakub Stępkowski <stepkos@wp.pl>
2025-05-28 03:12:16 +0200

    feat(instruments): implement Instrument model

---
+++ backend/modules/instruments/constants.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class InstrumentTypeEnum(models.TextChoices):
+    CRYPTO = 'CRYPTO', _('Crypto')
+    COMPANY = 'COMPANY', _('Company')
+    ETF = 'ETF', _('ETF')
+    INDEX = 'INDEX', _('Index')
+
+
+class FiatCurrencyEnum(models.TextChoices):
+    USD = 'USD', _('US Dollar')
+    EUR = 'EUR', _('Euro')
+    GBP = 'GBP', _('British Pound')
