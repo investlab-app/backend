a257251133c636889b58aac2293265a77d4fcc51
mkapica <mikolajkapica7@gmail.com>
2025-05-28 02:10:03 +0200

    Add websocket sanity checks

--- backend/modules/core/tests/conftest.py
+++ backend/modules/core/tests/conftest.py
@@ -18,3 +18,30 @@ def api_client_auth(user):
     client.force_authenticate(user=user)
     yield client
     client.logout()
+
+
+@pytest.fixture
+def websocket_communicator():
+    from channels.testing import WebsocketCommunicator
+
+    from config.asgi import application
+
+    async def _create_communicator(path="/ws/test/"):
+        communicator = WebsocketCommunicator(application, path)
+        return communicator
+
+    yield _create_communicator
+
+
+@pytest.fixture
+def websocket_communicator_auth(user):
+    from channels.testing import WebsocketCommunicator
+
+    from config.asgi import application
+
+    async def _create_communicator(path="/ws/test/"):
+        communicator = WebsocketCommunicator(application, path)
+        communicator.scope["user"] = user
+        return communicator
+
+    yield _create_communicator
