afd8f0b14690eb8f973e57fd82efdbd839dbcd86
Jakub Stępkowski <stepkos@wp.pl>
2025-05-28 03:12:16 +0200

    feat(instruments): implement Instrument model

--- backend/modules/instruments/admin.py
+++ backend/modules/instruments/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from django.utils.translation import gettext_lazy as _
+from modules.instruments.models import Instrument
 
-# Register your models here.
+
+@admin.register(Instrument)
+class InstrumentAdmin(ModelAdmin):
+    list_display = (
+        "ticker",
+        "type",
+        "name",
+        "currency",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("type", "currency")
+    search_fields = ("ticker", "name", "id")
+    readonly_fields = ("id", "created_at", "updated_at")
+    ordering = ("ticker",)
+    fieldsets = (
+        (None, {"fields": ("id", "ticker", "type", "name", "currency")}),
+        (
+            _("Description"),
+            {"fields": ("description",)},
+        ),
+        (
+            _("Timestamps"),
+            {"fields": ("created_at", "updated_at")},
+        ),
+    )
--- backend/modules/instruments/models.py
+++ backend/modules/instruments/models.py
@@ -1,3 +1,34 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from modules.core.models import BaseModel
+from modules.instruments.constants import InstrumentTypeEnum, FiatCurrencyEnum
+
+
+class Instrument(BaseModel):
+    type = models.CharField(
+        verbose_name=_("Type"), max_length=20, choices=InstrumentTypeEnum.choices
+    )
+    ticker = models.CharField(verbose_name=_("Ticker"), max_length=20, unique=True)
+    name = models.CharField(verbose_name=_("Name"), max_length=255)
+    description = models.TextField(verbose_name=_("Description"), blank=True)
+    currency = models.CharField(
+        verbose_name=_("Currency"), max_length=10, choices=FiatCurrencyEnum.choices
+    )
+
+    # detail_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # detail_object_id = models.UUIDField()
+    # detail = GenericForeignKey('detail_content_type', 'detail_object_id')
+
+    class Meta:
+        verbose_name = _("Instrument")
+        verbose_name_plural = _("Instruments")
+
+    def __str__(self):
+        return f"{self.ticker} - {self.name} ({self.type})"
+
+    def save(self, *args, **kwargs):
+        """Ensure ticker is always lowercase."""
+        self.ticker = self.ticker.lower()
+        super().save(*args, **kwargs)
