44a441deaecbd1ec9e1475d28cae95eed45951c2
Jakub Stępkowski <stepkos@wp.pl>
2025-05-28 04:06:29 +0200

    feat(instruments): implement details

--- backend/modules/instruments/models.py
+++ backend/modules/instruments/models.py
@@ -1,8 +1,13 @@
+import uuid
+
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 from modules.core.models import BaseModel
+from modules.core.utils import get_local_datetime
 from modules.instruments.constants import InstrumentTypeEnum, FiatCurrencyEnum
 
 
@@ -16,10 +21,23 @@ class Instrument(BaseModel):
     currency = models.CharField(
         verbose_name=_("Currency"), max_length=10, choices=FiatCurrencyEnum.choices
     )
+    # icon = models.ImageField()
 
-    # detail_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    # detail_object_id = models.UUIDField()
-    # detail = GenericForeignKey('detail_content_type', 'detail_object_id')
+    details_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.SET_NULL,
+        related_name="instruments",
+        verbose_name=_("Details Type"),
+        null=True,
+        blank=True,
+        limit_choices_to=Q(app_label='instruments', model__in=['companydetails', 'indexdetails']),
+    )
+    details_id = models.UUIDField(
+        verbose_name=_("Details ID"), blank=True, null=True, editable=True
+    )
+    details = GenericForeignKey('details_type', 'details_id')
+
+    # synchronized_at = models.DateTimeField(verbose_name=_("Synchronized at"), default=get_local_datetime)
 
     class Meta:
         verbose_name = _("Instrument")
@@ -28,7 +46,49 @@ class Instrument(BaseModel):
     def __str__(self):
         return f"{self.ticker} - {self.name} ({self.type})"
 
+    def clean(self):
+        """Check generic foreign key details."""
+        super().clean()
+        if self.details_type and not self.details_id:
+            raise ValueError(_("Details ID must be set when Details Type is provided."))
+        if not self.details_type and self.details_id:
+            raise ValueError(_("Details Type must be set when Details ID is provided."))
+
     def save(self, *args, **kwargs):
         """Ensure ticker is always lowercase."""
         self.ticker = self.ticker.lower()
         super().save(*args, **kwargs)
+
+
+class CompanyDetails(BaseModel):
+    name = models.CharField(verbose_name=_("Company Name"), max_length=255, unique=True)
+    country = models.CharField(
+        verbose_name=_("Country"), max_length=100, blank=True, null=True
+        # TODO: add enum
+    )
+    industry = models.CharField(
+        verbose_name=_("Industry"), max_length=100, blank=True, null=True
+        # TODO: add enum
+    )
+    ...  # Add fields according to what API provides
+    website = models.URLField(verbose_name=_("Website"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Company Details")
+        verbose_name_plural = _("Company Details")
+
+    def __str__(self):
+        return self.name
+
+
+class IndexDetails(BaseModel):
+    name = models.CharField(verbose_name=_("Index Name"), max_length=255, unique=True)
+    fund_name = models.CharField(verbose_name=_("Fund Name"), max_length=255, blank=True, null=True)
+    ...  # Add fields according to what API provides
+
+    class Meta:
+        verbose_name = _("Index Details")
+        verbose_name_plural = _("Index Details")
+
+    def __str__(self):
+        return f"{self.fund_name} - {self.name}"
