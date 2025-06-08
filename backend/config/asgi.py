81fa7bbe824d4d5dda50f89b19bc255903d3c0c9
mkapica <mikolajkapica7@gmail.com>
2025-06-09 00:07:20 +0200

    update

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -7,17 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+import os
+
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
 from config.urls import sse_urlpatterns
+
+http_application = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
         "http": URLRouter(
             sse_urlpatterns
-            + [re_path("^", get_asgi_application())]  # type: ignore [arg-type]
+            + [re_path("^", http_application)]  # type: ignore [arg-type]
         ),
     }
 )
