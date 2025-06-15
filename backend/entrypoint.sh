3b1fe84703ce3331d61fc56e104746190bb6da10
mkapica <mikolajkapica7@gmail.com>
2025-06-16 00:33:04 +0200

    Enhance Dockerfile and .dockerignore for improved development environment and dependency management. Updated Dockerfile to use a new base image and streamlined dependency installation. Expanded .dockerignore to exclude additional files and directories, optimizing build performance.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,15 +2,15 @@
 set -e  # Stop the script if any command fails
 
 echo "Migrating database"
-uv run manage.py migrate --noinput
+python manage.py migrate --noinput
 
 # Choose the server based on DEBUG
 if [[ "$DEBUG" =~ ^(1|true|True|TRUE|on|yes)$ ]]; then
   echo "Starting Django development server"
-  exec uv run manage.py runserver 0.0.0.0:8000
+  python manage.py runserver 0.0.0.0:8000
 else
   echo "Collecting static files"
-  uv run manage.py collectstatic --noinput
+  python manage.py collectstatic --noinput
   echo "Starting Uvicorn server"
   uvicorn config.asgi:application --host 0.0.0.0 --port 8000
 fi
