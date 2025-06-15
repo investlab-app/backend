472a6975993f9b3956a1dcc3518e76ca8154c1c5
mkapica <mikolajkapica7@gmail.com>
2025-06-15 15:06:55 +0200

    better caching and uv in dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,15 +2,15 @@
 set -e  # Stop the script if any command fails
 
 echo "Migrating database"
-python3 manage.py migrate --noinput
+uv run manage.py migrate --noinput
 
 # Choose the server based on DEBUG
 if [[ "$DEBUG" =~ ^(1|true|True|TRUE|on|yes)$ ]]; then
   echo "Starting Django development server"
-  exec python manage.py runserver 0.0.0.0:8000
+  exec uv run manage.py runserver 0.0.0.0:8000
 else
   echo "Collecting static files"
-  python3 manage.py collectstatic --noinput
+  uv run manage.py collectstatic --noinput
   echo "Starting Uvicorn server"
   uvicorn config.asgi:application --host 0.0.0.0 --port 8000
 fi
