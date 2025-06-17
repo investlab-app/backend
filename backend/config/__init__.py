cc08789990921b036cbbf2031b3aa686aeece128
mkapica <mikolajkapica7@gmail.com>
2025-06-17 13:17:26 +0200

    set settings modules

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,10 +1,14 @@
+import os
+
 import django
 
 from config.containers import setup_container
 from config.logging import setup_logging
 
+setup_logging()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
-setup_logging()
 
 container = setup_container()
