1f205edf5a080283f4c66db49cea9c2f25ea9b8c
Mikołaj Kapica <mikolajkapica7@gmail.com>
2025-08-13 10:37:16 +0200

    Add price info endpoint (#48)
    - single ticker
    - current price (min.close)
    - daily summary (day)

--- backend/modules/prices/urls.py
+++ backend/modules/prices/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from modules.prices.views import PricesV2View
+from modules.prices.views import PricesInfoView, PricesV2View
 
 urlpatterns = [
     path(
         "ohlc/",
         PricesV2View.as_view(),
         name="prices",
-    )
+    ),
+    path("<str:ticker>/", PricesInfoView.as_view(), name="prices-ticker"),
 ]
