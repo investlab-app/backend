f7b3a3d28b1c65b3c70cc522845e686ac23a7efa
Jakub Stępkowski <stepkos@wp.pl>
2025-08-22 01:11:48 +0200

    feat(instruments): implement PolygonInstrumentsRepository

--- backend/modules/instruments_v3/repositories.py
+++ backend/modules/instruments_v3/repositories.py
@@ -2,11 +2,11 @@ from typing import Iterator
 
 from config.settings import POLYGON_SECRET_KEY, POLYGON_EXCHANGE, POLYGON_ASSET_TYPE
 from polygon import RESTClient as PolygonClient
-from polygon.rest.models.tickers import Ticker
+from polygon.rest.models.tickers import Ticker, TickerDetails
 from urllib3 import HTTPResponse
 
 
-class SyncInstrumentsService:
+class PolygonInstrumentsRepository:
 
     def __init__(
         self, polygon_client: PolygonClient = None
@@ -14,14 +14,19 @@ class SyncInstrumentsService:
         self.polygon_client = polygon_client or PolygonClient(POLYGON_SECRET_KEY)
 
     def list_tickers(
-        self, market: str = None, exchange: str = None, limit: int = 100, *args, **kwargs
+        self,
+        limit: int = 100,
+        market: str = POLYGON_ASSET_TYPE,
+        exchange: str = POLYGON_EXCHANGE,
+        *args,
+        **kwargs,
     ) -> Iterator[Ticker]:
         """List tickers from Polygon API."""
 
         response = self.polygon_client.list_tickers(
-            market=market or POLYGON_ASSET_TYPE,
-            exchange=exchange or POLYGON_EXCHANGE,
             limit=limit,
+            market=market,
+            exchange=exchange,
             *args,
             **kwargs
         )
@@ -30,3 +35,12 @@ class SyncInstrumentsService:
 
         return response
 
+    def get_ticker_details(self, ticker: str) -> TickerDetails:
+        """Get details of a specific ticker from Polygon API."""
+        ticker = ticker.upper()
+
+        response = self.polygon_client.get_ticker_details(ticker)
+        if isinstance(response, HTTPResponse):
+            raise ValueError(f"Ticker {ticker} details incorrectly fetched from Polygon API")
+
+        return response
