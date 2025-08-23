dc0cb7c0b2a95bc521fb674c5862ce94833c311b
Jakub Stępkowski <stepkos@wp.pl>
2025-08-23 03:46:46 +0200

    feat(instruments): more abstraction

--- backend/modules/core/utils.py
+++ backend/modules/core/utils.py
@@ -5,21 +5,35 @@ from django.utils import timezone
 
 
 def get_local_datetime():
+    """Get the current local date and time."""
     return timezone.localtime(timezone.now())
 
 
 def get_local_date():
+    """Get the current local date."""
     return get_local_datetime().date()
 
 
 def get_local_time():
+    """Get the current local time."""
     return get_local_datetime().time()
 
 
 def get_random_bool():
+    """Return a random boolean value."""
     return random.choice([True, False])
 
 
 def quantize_decimal(value: Decimal, places: int = 15) -> Decimal:
+    """Quantize a Decimal value to a specified number of decimal places."""
     quant = Decimal(f"1e-{places}")
     return value.quantize(quant, rounding=ROUND_HALF_UP)
+
+
+def get_attr(obj, attr_path: str, scope_operator: str = '__'):
+    """Get nested attribute from an object using a scope operator."""
+    for attr in attr_path.split(scope_operator):
+        obj = getattr(obj, attr, None)
+        if obj is None:
+            return None
+    return obj
--- backend/modules/instruments_v3/services.py
+++ backend/modules/instruments_v3/services.py
@@ -1,11 +1,11 @@
 from typing import Iterable
 
+from modules.core.mixins import CreateWithMappingMixin, UpdateWithMappingMixin
 from modules.instruments_v3.models import Instrument
 from modules.instruments_v3.repositories import PolygonTickersRepository
-from polygon.rest.models.tickers import Ticker as TickerData, TickerDetails as TickerDetailsData
 
 
-class SyncInstrumentsBaseInfoService:
+class SyncInstrumentsBaseInfoService(CreateWithMappingMixin, UpdateWithMappingMixin):
     BATCH_SIZE = 500
     EDITABLE_FIELDS_MAPPING = {
         "active": "active",
@@ -15,14 +15,13 @@ class SyncInstrumentsBaseInfoService:
         "currency_symbol": "currency_symbol",
         "base_currency_symbol": "base_currency_symbol",
         "base_currency_name": "base_currency_name",
-        # "last_updated_utc": "last_updated_utc",
+        # "last_updated_utc": "last_updated_utc",  # TODO Decide what to do with this field
         "locale": "locale",
         "market": "market",
         "name": "name",
         "primary_exchange": "primary_exchange",
         "share_class_figi": "share_class_figi",
         "type": "type",
-        "source_feed": "source_feed",
     }
     CREATABLE_FIELDS_MAPPING = {
         "ticker": "ticker",
@@ -31,27 +30,6 @@ class SyncInstrumentsBaseInfoService:
 
     def __init__(self, repository: PolygonTickersRepository = None):
         self.repository = repository or PolygonTickersRepository()
-
-    def _update_instrument(
-        self, instrument: Instrument, ticker_data: TickerData
-    ) -> tuple[Instrument, bool]:
-        """Update existing instrument with new data."""
-        updated = False
-        for data_field, model_field in self.EDITABLE_FIELDS_MAPPING.items():
-            new_value = getattr(ticker_data, data_field)
-            if getattr(instrument, model_field) != new_value:
-                setattr(instrument, model_field, new_value)
-                updated = True
-        return instrument, updated
-
-    def _create_instrument(self, ticker_data: TickerData) -> Instrument:
-        """Create a new ticker from the provided data."""
-        return Instrument(
-            **{
-                model_field: getattr(ticker_data, data_field)
-                for data_field, model_field in self.CREATABLE_FIELDS_MAPPING.items()
-            }
-        )
 
     def sync_instruments(self) -> dict[str, int]:
         """Synchronize instruments base on Polygon Tickers."""
@@ -62,14 +40,14 @@ class SyncInstrumentsBaseInfoService:
         for ticker_data in tickers_data:
             if instrument := Instrument.objects.filter(ticker=ticker_data.ticker).first():
                 # Update existing instrument
-                updated_instrument, updated = self._update_instrument(instrument, ticker_data)
+                updated_instrument, updated = self.update_with_mapping(instrument, ticker_data)
                 if updated:
                     to_update.append(updated_instrument)
                 else:
                     no_changes += 1
             else:
                 # Create new ticker
-                new_instrument = self._create_instrument(ticker_data)
+                new_instrument = self.create_with_mapping(ticker_data, Instrument)
                 to_create.append(new_instrument)
 
         Instrument.objects.bulk_create(to_create, batch_size=self.BATCH_SIZE)
@@ -86,7 +64,7 @@ class SyncInstrumentsBaseInfoService:
         }
 
 
-class SyncInstrumentsDetailInfoService:
+class SyncInstrumentsDetailInfoService(UpdateWithMappingMixin):
     BATCH_SIZE = 500
     EDITABLE_FIELDS_MAPPING = {
         "active": "active",
@@ -134,27 +112,6 @@ class SyncInstrumentsDetailInfoService:
         self.instruments = instruments or Instrument.objects.all()
         self.repository = repository or PolygonTickersRepository()
 
-    @staticmethod
-    def _get_attr(obj, attr_path: str):
-        """Get nested attribute from an object using a double underscore path."""
-        for attr in attr_path.split('__'):
-            obj = getattr(obj, attr, None)
-            if obj is None:
-                return None
-        return obj
-
-    def _update_instrument(
-        self, instrument: Instrument, ticker_details_data: TickerDetailsData
-    ) -> tuple[Instrument, bool]:
-        """Update existing instrument with new data."""
-        updated = False
-        for data_field, model_field in self.EDITABLE_FIELDS_MAPPING.items():
-            new_value = self._get_attr(ticker_details_data, data_field)
-            if getattr(instrument, model_field) != new_value:
-                setattr(instrument, model_field, new_value)
-                updated = True
-        return instrument, updated
-
     def sync_instruments_details(self) -> dict[str, int]:
         """Synchronize instruments details based on Polygon TickerDetails."""
         to_update = []
@@ -167,7 +124,7 @@ class SyncInstrumentsDetailInfoService:
                 errors += 1
                 continue
 
-            updated_instrument, updated = self._update_instrument(instrument, ticker_details)
+            updated_instrument, updated = self.update_with_mapping(instrument, ticker_details)
             if updated:
                 to_update.append(updated_instrument)
             else:
