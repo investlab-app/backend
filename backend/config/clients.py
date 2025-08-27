f7e33c68c55c8e201d6a872bd94d00ccd40cce7c
Jakub Stępkowski <stepkos@wp.pl>
2025-08-28 01:55:20 +0200

    fix after rebase

--- backend/config/clients.py
+++ backend/config/clients.py
@@ -1,4 +1,9 @@
 from django.conf import settings
-from polygon import RESTClient
+from polygon import RESTClient, WebSocketClient
+from polygon.websocket.models import Feed, Market
 
 polygon_client = RESTClient(settings.POLYGON_SECRET_KEY)
+
+polygon_websocket_client = WebSocketClient(
+    api_key=settings.POLYGON_SECRET_KEY, feed=Feed.Delayed, market=Market.Stocks
+)
