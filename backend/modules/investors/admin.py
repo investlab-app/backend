d9fd4566397084e90f3448ba0fd35237e3977d22
Anna Sidor <annasidor00@gmail.com>
2025-09-03 13:08:14 +0200

    refactor: cleanup

--- backend/modules/investors/admin.py
+++ backend/modules/investors/admin.py
@@ -5,17 +5,10 @@ from modules.investors.models import Investor
 
 @admin.register(Investor)
 class InvestorAdmin(admin.ModelAdmin):
-    list_display = ["id",  "watching_instruments_count"]
-    # list_filter = ["user__clerk_role"]
-    # search_fields = ["user__email", "user__first_name", "user__last_name"]
+    list_display = ["clerk_id",  "watching_instruments_count"]
     filter_horizontal = ["watching_instruments"]
-
-    def user_email(self, obj):
-        return obj.user.email
-
-    user_email.short_description = "Email"  # type: ignore[attr-defined]
 
     def watching_instruments_count(self, obj):
         return obj.watching_instruments.count()
 
-    watching_instruments_count.short_description = "Watching Count"  # type: ignore[attr-defined]
+    watching_instruments_count.short_description = "Watching Count"
--- backend/modules/investors/models.py
+++ backend/modules/investors/models.py
@@ -3,10 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Investor(models.Model):
-    # user = models.OneToOneField("users.User", on_delete=models.CASCADE)
-    clerk_id = models.CharField(unique=True, max_length=255, verbose_name=_("Clerk ID"), null=True,
-    blank=True)
+    clerk_id = models.CharField(unique=True, max_length=255, verbose_name=_("Clerk ID"))
     watching_instruments = models.ManyToManyField("instruments.Instrument", blank=True)
 
     def __str__(self):
-        return f"Investor: {self.user.email}"
+        return f"Investor: {self.clerk_id}"
