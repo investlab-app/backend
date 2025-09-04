f7617749a5e7563a31d808eeba5d54f2c8e0578a
Jakub Stępkowski <stepkos@wp.pl>
2025-09-05 00:24:38 +0200

    feat(news): start module, implement news repository

---
+++ backend/modules/news/repositories.py
@@ -0,0 +1,45 @@
+from collections.abc import Iterator
+
+from polygon import RESTClient as PolygonClient
+from polygon.exceptions import BadResponse
+from polygon.rest.models.tickers import TickerNews
+
+from config.settings import POLYGON_SECRET_KEY
+
+
+class PolygonNewsRepository:
+    def __init__(self, polygon_client: PolygonClient = None):  # type: ignore
+        self.polygon_client = polygon_client or PolygonClient(POLYGON_SECRET_KEY)
+
+    def list_news(
+        self,
+        ticker: str = None,
+        published_utc: str = None,
+        published_utc_lt: str = None,
+        published_utc_lte: str = None,
+        published_utc_gt: str = None,
+        published_utc_gte: str = None,
+        sort: str = None,
+        order: str = None,
+        news_per_page: int = 30,
+        **kwargs,
+    ) -> Iterator[TickerNews] | None:
+        """List tickers from Polygon API.
+        - default `sort` is by published_utc
+        - default `order` is desc
+        """
+        try:
+            return self.polygon_client.list_ticker_news(
+                ticker=ticker.upper() if ticker else None,
+                published_utc=published_utc,
+                published_utc_lt=published_utc_lt,
+                published_utc_lte=published_utc_lte,
+                published_utc_gt=published_utc_gt,
+                published_utc_gte=published_utc_gte,
+                sort=sort,
+                order=order,
+                limit=news_per_page,  # Max news per one request
+                **kwargs,
+            )
+        except BadResponse:
+            return None
