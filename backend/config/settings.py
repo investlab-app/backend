985ba3d1dd59b808c4e3c95a9c7be06990d37bd4
mkapica <mikolajkapica7@gmail.com>
2025-10-06 22:09:13 +0200

    better redis config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -354,17 +354,22 @@ UNFOLD = {
     # ],
 }
 
+REDIS_HOST = os.environ["REDIS_HOST"]
+REDIS_PORT = os.environ["REDIS_PORT"]
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}"
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis-master", 6379)],
+            "hosts": [REDIS_URL],
         },
     },
 }
 
 # Celery settings
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://redis-master:6379/0")
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", f"{REDIS_URL}/0")
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_RESULT_EXTENDED = True
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
