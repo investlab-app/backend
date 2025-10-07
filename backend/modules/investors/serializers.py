35db88cf1614228154339f8b03ab1a7b018687e9
Jakub Stępkowski <stepkos@wp.pl>
2025-10-07 08:59:29 +0200

    feat: implement me/account-value

--- backend/modules/investors/serializers.py
+++ backend/modules/investors/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
 from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
+
 from modules.investors.models import AccountValueSnapshot, Asset, Investor
 
 
