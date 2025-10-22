e135df1315e208aa4b26b9727219434c5b35d78a
mkapica <mikolajkapica7@gmail.com>
2025-10-22 17:40:18 +0200

    add backend-order-engine

--- backend/modules/orders/services.py
+++ backend/modules/orders/services.py
@@ -40,9 +40,9 @@ class RunOrderEngineService:
             prices = self.price_listener.get_prices()
             data.prices = prices
 
-            output = self.engine.run(prices)
+            output = self.engine.run(data)
 
-            await self.output_handler.handle(output)
+            await self.output_handler.handle(output, prices)
 
 
 class TradeEngineDataFetcher:
