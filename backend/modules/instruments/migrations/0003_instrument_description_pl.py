9ce9062dd5b5f49956036d148aa328284c889807
mkapica <mikolajkapica7@gmail.com>
2025-11-03 23:02:31 +0100

    start

--- backend/modules/instruments/migrations/0003_instrument_description_pl.py
+++ backend/modules/instruments/migrations/0003_instrument_description_pl.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('instruments', '0001_initial'),
+        ('instruments', '0002_alter_instrument_active_and_more'),
     ]
 
     operations = [
