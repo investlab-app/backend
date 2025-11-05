3c01499d298280accfd96bc0e8d8abc204ac4684
Jakub Stępkowski <stepkos@wp.pl>
2025-11-05 01:38:20 +0100

    feat(orders): implement blocking funds when order is open (#124)
    * feat(orders): implement blocking funds when order is open
    
    * fix(orders): update tests
    
    * fix(orders): fix pipeline
    
    * feat(orders): handle blocked funds
    
    * style: format code
    
    * chore: generate schema.yml

--- backend/modules/orders/views.py
+++ backend/modules/orders/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from modules.investors.models import Investor
 from modules.orders.models import Order
 from modules.orders.serializers import CreateMarketOrderSerializer, OrderSerializer
+from modules.orders.services.order_services import MarketOrderService
 
 
 class CreateMarketOrderView(generics.CreateAPIView):
@@ -29,3 +30,7 @@ class DestroyOrderView(generics.DestroyAPIView):
     def get_queryset(self):
         investor = get_object_or_404(Investor, clerk_id=self.request.user.id)
         return Order.objects.filter(investor=investor)
+
+    def perform_destroy(self, instance):
+        service = MarketOrderService()
+        service.delete(instance)
