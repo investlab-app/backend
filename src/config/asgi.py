0e3d83728b416250b6630d79f2fac2d0eaeb8ed0
Jakub Stępkowski <stepkos@wp.pl>
2025-11-12 23:11:19 +0100

    chore: adjust conf files

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
