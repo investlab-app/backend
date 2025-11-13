f4fe1823f1bd77a1e2dd8f69ab87647b8fdac4fc
Mikołaj Kapica <mikolajkapica7@gmail.com>
2025-11-13 12:17:57 +0100

    feat/pending orders (#133)
    * save
    
    * save
    
    * save
    
    * fmt, lint, resolve type warnings
    
    * fix tests
    
    * update schema

--- backend/modules/instruments/serializers.py
+++ backend/modules/instruments/serializers.py
@@ -83,9 +83,3 @@ class InstrumentWithPriceSerializer(InstrumentListSerializer):
         if not investor:
             return False
         return investor.watching_instruments.filter(id=obj.id).exists()
-
-
-class InstrumentNameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Instrument
-        fields = ["ticker"]
