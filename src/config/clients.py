801b417ac274e281c901bd75dd5005bec465c305
Jakub Stępkowski <stepkos@wp.pl>
2025-11-15 19:24:27 +0100

    Fixes after rebase

--- src/config/clients.py
+++ src/config/clients.py
@@ -1,9 +1,9 @@
 import redis
+from clerk_backend_api import Clerk
 from django.conf import settings
 from openai import OpenAI
 from polygon import RESTClient, WebSocketClient
 from polygon.websocket.models import Feed, Market
-from clerk_backend_api import Clerk
 
 clerk_client = Clerk(bearer_auth=settings.CLERK_SECRET_KEY)
 
