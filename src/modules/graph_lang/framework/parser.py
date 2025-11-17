7b074d2f63615f0cf5786f21de3db6d1858d9073
Krzyzan <johndoe@example.com>
2025-11-17 11:23:30 +0100

    Added two fundamentals nodes

--- src/modules/graph_lang/framework/parser.py
+++ src/modules/graph_lang/framework/parser.py
@@ -45,6 +45,10 @@ class Parser:
                 unit = data.pop("unit")
                 period = data.pop("interval")
                 data["timespan"] = f"{period} {unit}"
+            if 'unit2' in data and 'interval2' in data:
+                unit2 = data.pop("unit2")
+                period2 = data.pop("interval2")
+                data["timespan2"] = f"{period2} {unit2}"
             nodes.append(
                 NodeData(
                     id=node_json["id"],
