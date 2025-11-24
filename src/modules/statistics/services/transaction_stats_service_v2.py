ede2f189ef7826c493b74ea8d783b23d0fb45a15
Jakub Stępkowski <stepkos@wp.pl>
2025-11-25 00:14:38 +0100

    feat: standardize compute_stats outputs

--- src/modules/statistics/services/transaction_stats_service_v2.py
+++ src/modules/statistics/services/transaction_stats_service_v2.py
@@ -115,7 +115,13 @@ class TransactionStatsService:
             },
         }
 
-    def compute_stats_in_period(self, start: datetime, end: datetime) -> dict[str, Any]:
+    def compute_stats_in_period(
+        self, start: datetime, end: datetime, current_price: Decimal | None = None
+    ) -> dict[str, Any]:
+        if current_price is None:
+            prices = self.lastest_price_service.get_prices_default_dict()
+            current_price = prices[self.ticker]
+
         pre_txs = [t for t in self.transactions if t.timestamp < start]
         pre_service = TransactionStatsService(
             pre_txs,
@@ -123,19 +129,15 @@ class TransactionStatsService:
             lastest_price_service=self.lastest_price_service,
         )
         pre_stats = pre_service.compute_stats(current_price=Decimal(0))
-        initial_lots = [
-            BuyLot(volume=i["volume"], price=i["buy_price"], timestamp=None)
-            for i in pre_stats["details"]["remaining_lots_fifo_order"]
+
+        buy_lots: list[BuyLot] = [
+            BuyLot(volume=i['volume'], price=i['buy_price'], timestamp=None)
+            for i in pre_stats['details']['remaining_lots_fifo_order']
         ]
-
+        total_buy_cost = sum(i.volume * i.price for i in buy_lots)
         window_txs = [t for t in self.transactions if start <= t.timestamp <= end]
-        buy_lots = initial_lots.copy()
         realized_gain = Decimal(0)
-        total_buy_cost = sum(i.volume * i.price for i in initial_lots)
-        sell_details = []
-
-        prices = self.lastest_price_service.get_prices_default_dict()
-        current_price = prices[self.ticker]
+        sell_details: list[dict[str, Any]] = []
 
         for tx in window_txs:
             vol, price = tx.volume, tx.price
@@ -157,19 +159,20 @@ class TransactionStatsService:
                     sell_volume -= take
                     if lot.volume == 0:
                         buy_lots.pop(0)
-                realized_gain += consumed_volume * price - consumed_cost
-                sell_details.append(
-                    {
-                        "volume": consumed_volume,
-                        "sell_price": price,
-                        "cost_basis": consumed_cost,
-                        "gain": consumed_volume * price - consumed_cost,
-                    }
-                )
+                gain = consumed_volume * price - consumed_cost
+                realized_gain += gain
+                sell_details.append({
+                    "volume": consumed_volume,
+                    "sell_price": price,
+                    "cost_basis": consumed_cost,
+                    "gain": gain,
+                    "gain_pct": (gain / consumed_cost) if consumed_cost else None,
+                })
 
+        remaining_volume = sum(i.volume for i in buy_lots)
         unrealized_gain = sum(i.volume * (current_price - i.price) for i in buy_lots)
         total_gain = realized_gain + unrealized_gain
-        total_gain_pct = total_gain / total_buy_cost if total_buy_cost else None
+        total_gain_pct = (total_gain / total_buy_cost) if total_buy_cost else None
 
         return {
             "realized_gain": realized_gain,
@@ -177,15 +180,45 @@ class TransactionStatsService:
             "total_gain": total_gain,
             "total_buy_cost": total_buy_cost,
             "total_gain_pct": total_gain_pct,
-            "remaining_volume": sum(i.volume for i in buy_lots),
             "current_price": current_price,
+            "remaining_volume": remaining_volume,
             "details": {
                 "sells": sell_details,
-                "initial_lots": [
-                    {"volume": i.volume, "price": i.price} for i in initial_lots
+                "remaining_lots_lifo_order": [
+                    {
+                        "volume": i.volume,
+                        "buy_price": i.price,
+                        "contribution": i.volume * (current_price - i.price),
+                    }
+                    for i in reversed(buy_lots)
                 ],
-                "ending_lots": [
-                    {"volume": i.volume, "price": i.price} for i in buy_lots
+                "remaining_lots_fifo_order": [
+                    {"volume": i.volume, "buy_price": i.price} for i in buy_lots
                 ],
             },
         }
+#
+#
+# class MultipleInstrumentStatsService:
+#     def __init__(
+#         self,
+#         investor: Investor,
+#         instruments: list[Instrument],
+#         lastest_price_service: LatestPriceService | None = None,
+#     ):
+#         self.investor = investor
+#         self.instruments = instruments
+#         self.lastest_price_service = lastest_price_service or LatestPriceService()
+#
+#     def get_stats(self) -> dict[str, dict[str, Any]]:
+#         stats = {}
+#         prices = self.lastest_price_service.get_prices_default_dict()
+#         for instrument in self.instruments:
+#             svc = TransactionStatsService.from_investor_and_instrument(
+#                 self.investor, instrument
+#             )
+#             instrument_stats = svc.compute_stats(
+#                 current_price=prices[instrument.ticker]
+#             )
+#             stats[instrument.ticker] = instrument_stats
+#         return stats
