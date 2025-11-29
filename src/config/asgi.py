1ff17260b0951d52caf9eaee260ae09533bf970b
Mikołaj Kapica <mikolajkapica7@gmail.com>
2025-11-29 16:56:03 +0100

    fix: remove calling django.setup directly (#186)
    * fix/remove-calling-django-setup-directly
    
    * fix/remove-calling-django-setup-directly

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,12 +1,18 @@
+import os
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from config.urls import websocket_urlpatterns
-from modules.authentication.middlewares import CookieWebsocketAuthMiddleware
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 http_application = get_asgi_application()
+
+from config.urls import websocket_urlpatterns  # noqa: E402
+from modules.authentication.middlewares import (  # noqa: E402
+    CookieWebsocketAuthMiddleware,
+)
 
 application = ProtocolTypeRouter(
     {
