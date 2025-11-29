d69cf73380b6ea9e0f4161b9e5d5b8a24626f2fc
Jakub Stępkowski <stepkos@wp.pl>
2025-11-29 02:05:27 +0100

    feat: change TransactionHistoryView share_price and acquisition_price

--- src/modules/statistics/views.py
+++ src/modules/statistics/views.py
@@ -409,7 +409,8 @@ class TransactionHistoryView(generics.RetrieveAPIView):
                     "share_price": round(transaction.price, 2),
                     "acquisition_price": (
                         round(transaction.volume * transaction.price, 2)
-                        if transaction.is_buy else 0
+                        if transaction.is_buy
+                        else 0
                     ),
                 }
                 history.append(history_entry)
