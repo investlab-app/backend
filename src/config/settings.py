17e0a2d48db687e6b38e1d69152d12866bd8f280
Jakub Stępkowski <stepkos@wp.pl>
2025-12-09 19:47:05 +0100

    chore: clean unused djnago settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,9 +5,6 @@ from celery.schedules import crontab
 
 from config.utils import str_to_bool, str_to_list
 
-# from django.templatetags.static import static
-# from django.utils.translation import gettext_lazy as _
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["SECRET_KEY"]
@@ -23,13 +20,7 @@ CSRF_TRUSTED_ORIGINS = str_to_list(os.environ.get("CSRF_TRUSTED_ORIGINS", ""))
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",  # Django admin theme
-    # "unfold.contrib.filters",  # optional, for special filters
-    # "unfold.contrib.forms",  # optional, for special form elements
-    # "unfold.contrib.inlines",  # optional, for special inlines
-    # "unfold.contrib.import_export"  # optional, for django-import-export
-    # "unfold.contrib.guardian",  # optional, for django-guardian
-    # "unfold.contrib.simple_history",  # optional, for django-simple-history
+    "unfold",  # Django admin theme, should be first
     # Django modules
     "django.contrib.admin",
     "django.contrib.auth",
@@ -252,43 +243,24 @@ SPECTACULAR_SETTINGS = {
     },
 }
 
-# SIMPLE_JWT = {
-#     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
-#     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
-# }
-
 UNFOLD = {
     "SITE_TITLE": "Stocks",
     "SITE_HEADER": "Stocks Admin Panel",
-    # "SITE_SUBHEADER": "Appears under SITE_HEADER",
     "SITE_DROPDOWN": [
         {
             "icon": "folder",
             "title": "Github",
-            "link": "https://github.com/wall-street-stocks/stocks-backend",
-        },
-        {
-            "icon": "dashboard",
-            "title": "Frontend",
-            "link": "https://example.com",
+            "link": "https://github.com/investlab-app/backend/",
         },
     ],
-    # "SITE_URL": "/",
-    # # "SITE_ICON": (
-    #   lambda request: static("icon.svg"),  # both modes, optimise for 32px height
-    #  )
     # "SITE_ICON": {
     #     "light": lambda request: static("icon-light.svg"),  # light mode
     #     "dark": lambda request: static("icon-dark.svg"),  # dark mode
     # },
-    # # "SITE_LOGO": (
-    #   lambda request: static("logo.svg"),  # both modes, optimise for 32px height
-    # )
     # "SITE_LOGO": {
     #     "light": lambda request: static("logo-light.svg"),  # light mode
     #     "dark": lambda request: static("logo-dark.svg"),  # dark mode
     # },
-    # "SITE_SYMBOL": "speed",  # symbol from icon set
     # "SITE_FAVICONS": [
     #     {
     #         "rel": "icon",
@@ -297,103 +269,12 @@ UNFOLD = {
     #         "href": lambda request: static("favicon.svg"),
     #     },
     # ],
-    # "SHOW_HISTORY": True,  # show/hide "History" button, default: True
-    # "SHOW_VIEW_ON_SITE": True,  # show/hide "View on site" button, default: True
-    # "SHOW_BACK_BUTTON": (
-    #    False,  # show/hide "Back" button on changeform in header, default: False
-    # )
-    # "ENVIRONMENT": "sample_app.environment_callback",  # environment name in header
-    # # environment name prefix in title tag
-    # "ENVIRONMENT_TITLE_PREFIX": "sample_app.environment_title_prefix_callback",
-    # "DASHBOARD_CALLBACK": "sample_app.dashboard_callback",
-    # "LOGIN": {
-    #     "image": lambda request: static("sample/login-bg.jpg"),
-    #     "redirect_after": lambda request: reverse_lazy("admin:APP_MODEL_changelist"),
-    # },
-    # "COLORS": {
-    #     "base": {
-    #         "50": "249, 250, 251",
-    #         "100": "243, 244, 246",
-    #         "200": "229, 231, 235",
-    #         "300": "209, 213, 219",
-    #         "400": "156, 163, 175",
-    #         "500": "107, 114, 128",
-    #         "600": "75, 85, 99",
-    #         "700": "55, 65, 81",
-    #         "800": "31, 41, 55",
-    #         "900": "17, 24, 39",
-    #         "950": "3, 7, 18",
-    #     },
-    #     "primary": {
-    #         "50": "250, 245, 255",
-    #         "100": "243, 232, 255",
-    #         "200": "233, 213, 255",
-    #         "300": "216, 180, 254",
-    #         "400": "192, 132, 252",
-    #         "500": "168, 85, 247",
-    #         "600": "147, 51, 234",
-    #         "700": "126, 34, 206",
-    #         "800": "107, 33, 168",
-    #         "900": "88, 28, 135",
-    #         "950": "59, 7, 100",
-    #     },
-    #     "font": {
-    #         "subtle-light": "var(--color-base-500)",  # text-base-500
-    #         "subtle-dark": "var(--color-base-400)",  # text-base-400
-    #         "default-light": "var(--color-base-600)",  # text-base-600
-    #         "default-dark": "var(--color-base-300)",  # text-base-300
-    #         "important-light": "var(--color-base-900)",  # text-base-900
-    #         "important-dark": "var(--color-base-100)",  # text-base-100
-    #     },
-    # },
-    # "EXTENSIONS": {
-    #     "modeltranslation": {
-    #         "flags": {
-    #             "en": "🇬🇧",
-    #             "fr": "🇫🇷",
-    #             "nl": "🇧🇪",
-    #         },
-    #     },
-    # },
+    "SHOW_HISTORY": True,
+    "SHOW_VIEW_ON_SITE": False,
     "SIDEBAR": {
         "show_search": True,  # Search in applications and models names
         "show_all_applications": False,  # Dropdown with all applications and models
-        # "navigation": [
-        #     {
-        #         "title": _("Navigation"),
-        #         "separator": True,  # Top border
-        #         "collapsible": True,  # Collapsible group of links
-        #         "items": [
-        #             {
-        #                 "title": _("Dashboard"),
-        #                 "icon": "dashboard",  # Supported icon set: https://fonts.google.com/icons
-        #                 "link": reverse_lazy("admin:index"),
-        #                 "badge": "sample_app.badge_callback",
-        #                 "permission": lambda request: request.user.is_superuser,
-        #             },
-        #             {
-        #                 "title": _("Users"),
-        #                 "icon": "people",
-        #                 "link": reverse_lazy("admin:auth_user_changelist"),
-        #             },
-        #         ],
-        #     },
-        # ],
     },
-    # "TABS": [
-    #     {
-    #         "models": [
-    #             "app_label.model_name_in_lowercase",
-    #         ],
-    #         "items": [
-    #             {
-    #                 "title": _("Your custom title"),
-    #                 "link": reverse_lazy("admin:app_label_model_name_changelist"),
-    #                 "permission": "sample_app.permission_callback",
-    #             },
-    #         ],
-    #     },
-    # ],
 }
 
 # Redis
